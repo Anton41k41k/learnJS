4a3d9d1ead156a23fb365b0cb55d8d1dc506ea00
Anton <you@example.com>
2023-10-26 20:29:46 +0300

    todolist_finishVersion

--- ToDoList.js
+++ ToDoList.js
@@ -5,24 +5,25 @@ const list = {
 };
 
 function changeStatus(task, status) {
+  if (task in list) {
 
- if (status == list[task]){
+    list[task] = status;
 
-  console.log(`У "${task}" уже задан статус "${status}"`);
+ } else if (status == list[task]){
+
+  console.log(`У "${task}" уже задан статус "${status}".`);
+
+ } else {
+
+  console.log(`Задачи "${task}" не существует, либо вы ввели не верное название.`);
 
  } 
- 
- else {
-
-  list[task] = status;
-
- }
 
 }
 
 function addTask(task) {
   if (task in list) {
-    console.log(`Задача "${task}" уже есть в списке дел`);
+    console.log(`Задача "${task}" уже есть в списке дел.`);
   } else{
     list[task] = "";
   }
@@ -32,7 +33,7 @@ function deleteTask(task){
   if (task in list) {
     delete list[task];
   } else {
-    console.log("Вы неверно ввели задачу, либо ее не существует");
+    console.log("Вы неверно ввели задачу, либо ее не существует.");
   }
     
 
@@ -46,7 +47,7 @@ function showList() {
 for (key in list){
   if (list[key] == "To Do"){
     ToDo[key] = key;
-  }
+  } 
 
   if (list[key] == "In Progress"){
     In_Progress[key] = key;
@@ -54,18 +55,28 @@ for (key in list){
 
   if (list[key] == "Done"){
     Done[key] = key;
-  } 
-}
-console.log(`ToDo: ${Object.keys(ToDo)}, In_Progress: ${Object.keys(In_Progress)}, Done: ${Object.keys(Done)}`);
+  }
+
+  
+
 }
 
-changeStatus("write a post", "In Progress");
-deleteTask("make a bed");
-addTask("Выйти в окно");
-changeStatus("Выйти в окно", "In Progress");
-changeStatus("write a post", "In Progress");
-changeStatus("write a post", "Done");
-addTask("play in Dota 2");
-changeStatus("play in Dota 2", "To Do") 
+if (!Object.keys(ToDo).length){
+    ToDo["-"] = '-';
+  }
+if (!Object.keys(In_Progress).length){
+    In_Progress["-"] = '-';
+  }
+if (!Object.keys(Done).length){
+    Done["-"] = '-';
+  }
+
+console.log(`ToDo: ${Object.keys(ToDo)}, In_Progress: ${Object.keys(In_Progress)}, Done: ${Object.keys(Done)}`);
+
+}
+
+changeStatus("write a posd", "Done");
+changeStatus("make a bed", "To Do");
+changeStatus("create a new practice task", "To Do");
 
 showList();
