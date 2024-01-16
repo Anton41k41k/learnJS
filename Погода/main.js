3d9b4d9d29bffd7524671a2990b532ab6a2f4033
Anton <you@example.com>
2024-01-16 17:38:58 +0300

    add filter and promise

--- Погода/main.js
+++ Погода/main.js
@@ -1,25 +1,16 @@
 import {toStorageFollow,toStorageNow} from "./module.js";
 
-export const serverUrl = 'http://api.openweathermap.org/data/2.5/weather';
+const serverUrl = 'http://api.openweathermap.org/data/2.5/weather';
 const cityName = document.querySelector('.search-input');
-export const apiKey = 'f660a2fb1e4bad108d6160b7f58c555f&units=metric';
-export const tempText = document.querySelector('.temp__text');
-export const cityNow = document.querySelector('.city__now');
+const apiKey = 'f660a2fb1e4bad108d6160b7f58c555f&units=metric';
+const tempText = document.querySelector('.temp__text');
+const cityNow = document.querySelector('.city__now');
 const listCity = document.querySelector('.list__city');
 const followBtn = document.querySelector('.followBtn');
-export const imgCloud = document.querySelector('.img__cloud');
+const imgCloud = document.querySelector('.img__cloud');
 
-function now(event) {
-  const url = `${serverUrl}?q=${cityName.value}&appid=${apiKey}`;
-
-  (async() => {
-    let cityTemp = await fetch(url);
-    let temp = await cityTemp.json();
-
-    tempText.textContent = Math.round(temp.main.temp);
-    cityNow.textContent = temp.name;
-
-    switch (temp.weather[0].description) {
+function filter(object) {
+  switch (object) {
       case 'clear sky':
         imgCloud.src = './img/clouds/clearSky.png';
         break;
@@ -96,9 +87,31 @@ function now(event) {
         imgCloud.src = './img/clouds/mist.png';
         break;
     }
-    toStorageNow(cityNow);
-  })()
+}
 
+
+
+
+function now(event) {
+  const url = `${serverUrl}?q=${cityName.value}&appid=${apiKey}`;
+  const search = new Promise(async(resolve, reject) => {
+    let cityTemp = await fetch(url);
+    let temp = await cityTemp.json();
+    resolve(temp);
+  })
+
+  search.then(temp => {
+    return new Promise((resolve, reject) => {
+      tempText.textContent = Math.round(temp.main.temp);
+      cityNow.textContent = temp.name;
+
+      filter (temp.weather[0].description);
+      toStorageNow(cityNow);
+      resolve()
+    })
+    
+  })
+  
   .catch(error => {
     alert('ошибка');
     tempText.textContent = '-';
@@ -129,97 +142,26 @@ function getTemp(event) {
   } else{
     const url = `${serverUrl}?q=${event.target.textContent}&appid=${apiKey}`;
 
-    (async() => {
+    const search = new Promise(async(resolve, reject) => {
+    let cityTemp = await fetch(url);
+    let temp = await cityTemp.json();
+    resolve(temp);
+    })
+
+    search.then(temp => {
+      return new Promise((resolve, reject) => {
+        tempText.textContent = Math.round(temp.main.temp);
+        cityNow.textContent = temp.name;
+
+        filter (temp.weather[0].description);
+        toStorageNow(cityNow);
+        resolve()
+      })
+    
+    })
   
-      let cityTemp = await fetch(url);
-      let temp = await cityTemp.json();
-
-      tempText.textContent = Math.round(temp.main.temp);
-      cityNow.textContent = temp.name;
-
-      switch (temp.weather[0].description) {
-      case 'clear sky':
-        imgCloud.src = './img/clouds/clearSky.png';
-        break;
-      case 'few clouds':
-        imgCloud.src = './img/clouds/fewClouds.png';
-        break;
-      case 'scattered clouds':
-        imgCloud.src = './img/clouds/scatteredClouds.png';
-        break;
-      case 'broken clouds':
-      case 'overcast clouds':
-        imgCloud.src = './img/clouds/brokenClouds.png';
-        break;
-      case 'light intensity drizzle':
-      case 'drizzle':
-      case 'light intensity shower rain':
-      case 'shower rain':
-      case 'heavy intensity shower rain':
-      case 'ragged shower rain':
-      case 'heavy intensity drizzle':
-      case 'light intensity drizzle rain':
-      case 'drizzle rain':
-      case 'heavy intensity drizzle rain':
-      case 'shower rain and drizzle':
-      case 'heavy shower rain and drizzle':
-      case 'shower drizzle':
-      case 'shower rain':
-        imgCloud.src = './img/clouds/showerRain.png';
-        break;
-      case 'light rain':
-      case 'moderate rain':
-      case 'heavy intensity rain':
-      case 'very heavy rain':
-      case 'extreme rain':
-      case 'rain':
-        imgCloud.src = './img/clouds/rain.png';
-        break;
-      case 'thunderstorm with light rain':
-      case 'thunderstorm with rain':
-      case 'thunderstorm with heavy rain':
-      case 'light thunderstorm':
-      case 'thunderstorm':
-      case 'heavy thunderstorm':
-      case 'ragged thunderstorm':
-      case 'thunderstorm with light drizzle':
-      case 'thunderstorm with drizzle':
-      case 'thunderstorm with heavy drizzle':
-        imgCloud.src = './img/clouds/thunderstorm.png';
-        break;
-      case 'freezing rain':
-      case 'light snow':
-      case 'snow':
-      case 'heavy snow':
-      case 'sleet':
-      case 'light shower sleet':
-      case 'shower sleet':
-      case 'light rain and snow':
-      case 'rain and snow':
-      case 'light shower snow':
-      case 'shower snow':
-      case 'heavy shower snow':
-        imgCloud.src = './img/clouds/snow.png';
-        break;
-      case 'mist':
-      case 'smoke':
-      case 'haze':
-      case 'sand/dust whirls':
-      case 'fog':
-      case 'sand':
-      case 'dust':
-      case 'volcanic ash':
-      case 'squalls':
-      case 'tornado':
-        imgCloud.src = './img/clouds/mist.png';
-        break;
-    }
-
-    toStorageNow(cityNow);
-    })()
-
     .catch(error => {
-      alert(`Ошибка`);
+      alert('ошибка');
       tempText.textContent = '-';
       cityNow.textContent = 'Ошибка!';
     });
@@ -228,93 +170,24 @@ function getTemp(event) {
 
 function reloadPage() {
   const url = `${serverUrl}?q=${localStorage.getItem('city')}&appid=${apiKey}`;
-
-  (async() => {
+  const search = new Promise(async(resolve, reject) => {
     let cityTemp = await fetch(url);
     let temp = await cityTemp.json();
+    resolve(temp);
+  })
 
-    tempText.textContent = Math.round(temp.main.temp);
-    cityNow.textContent = temp.name;
+  search.then(temp => {
+    return new Promise((resolve, reject) => {
+      tempText.textContent = Math.round(temp.main.temp);
+      cityNow.textContent = temp.name;
 
-    switch (temp.weather[0].description) {
-      case 'clear sky':
-        imgCloud.src = './img/clouds/clearSky.png';
-        break;
-      case 'few clouds':
-        imgCloud.src = './img/clouds/fewClouds.png';
-        break;
-      case 'scattered clouds':
-        imgCloud.src = './img/clouds/scatteredClouds.png';
-        break;
-      case 'broken clouds':
-      case 'overcast clouds':
-        imgCloud.src = './img/clouds/brokenClouds.png';
-        break;
-      case 'light intensity drizzle':
-      case 'drizzle':
-      case 'light intensity shower rain':
-      case 'shower rain':
-      case 'heavy intensity shower rain':
-      case 'ragged shower rain':
-      case 'heavy intensity drizzle':
-      case 'light intensity drizzle rain':
-      case 'drizzle rain':
-      case 'heavy intensity drizzle rain':
-      case 'shower rain and drizzle':
-      case 'heavy shower rain and drizzle':
-      case 'shower drizzle':
-      case 'shower rain':
-        imgCloud.src = './img/clouds/showerRain.png';
-        break;
-      case 'light rain':
-      case 'moderate rain':
-      case 'heavy intensity rain':
-      case 'very heavy rain':
-      case 'extreme rain':
-      case 'rain':
-        imgCloud.src = './img/clouds/rain.png';
-        break;
-      case 'thunderstorm with light rain':
-      case 'thunderstorm with rain':
-      case 'thunderstorm with heavy rain':
-      case 'light thunderstorm':
-      case 'thunderstorm':
-      case 'heavy thunderstorm':
-      case 'ragged thunderstorm':
-      case 'thunderstorm with light drizzle':
-      case 'thunderstorm with drizzle':
-      case 'thunderstorm with heavy drizzle':
-        imgCloud.src = './img/clouds/thunderstorm.png';
-        break;
-      case 'freezing rain':
-      case 'light snow':
-      case 'snow':
-      case 'heavy snow':
-      case 'sleet':
-      case 'light shower sleet':
-      case 'shower sleet':
-      case 'light rain and snow':
-      case 'rain and snow':
-      case 'light shower snow':
-      case 'shower snow':
-      case 'heavy shower snow':
-        imgCloud.src = './img/clouds/snow.png';
-        break;
-      case 'mist':
-      case 'smoke':
-      case 'haze':
-      case 'sand/dust whirls':
-      case 'fog':
-      case 'sand':
-      case 'dust':
-      case 'volcanic ash':
-      case 'squalls':
-      case 'tornado':
-        imgCloud.src = './img/clouds/mist.png';
-        break;
-    }
-  })()
-
+      filter (temp.weather[0].description);
+      toStorageNow(cityNow);
+      resolve()
+    })
+    
+  })
+  
   .catch(error => {
     alert('ошибка');
     tempText.textContent = '-';
