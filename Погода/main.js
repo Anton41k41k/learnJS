2e2900e4bcb13c11c8f1b49db8ac35909f1355ef
Anton <you@example.com>
2024-01-16 18:50:38 +0300

    moved the filter to the module

--- Погода/main.js
+++ Погода/main.js
@@ -1,4 +1,4 @@
-import {toStorageFollow,toStorageNow} from "./module.js";
+import {toStorageFollow,toStorageNow, imgFilter} from "./module.js";
 
 const serverUrl = 'http://api.openweathermap.org/data/2.5/weather';
 const cityName = document.querySelector('.search-input');
@@ -8,89 +8,6 @@ const cityNow = document.querySelector('.city__now');
 const listCity = document.querySelector('.list__city');
 const followBtn = document.querySelector('.followBtn');
 const imgCloud = document.querySelector('.img__cloud');
-
-function filter(object) {
-  switch (object) {
-      case 'clear sky':
-        imgCloud.src = './img/clouds/clearSky.png';
-        break;
-      case 'few clouds':
-        imgCloud.src = './img/clouds/fewClouds.png';
-        break;
-      case 'scattered clouds':
-        imgCloud.src = './img/clouds/scatteredClouds.png';
-        break;
-      case 'broken clouds':
-      case 'overcast clouds':
-        imgCloud.src = './img/clouds/brokenClouds.png';
-        break;
-      case 'light intensity drizzle':
-      case 'drizzle':
-      case 'light intensity shower rain':
-      case 'shower rain':
-      case 'heavy intensity shower rain':
-      case 'ragged shower rain':
-      case 'heavy intensity drizzle':
-      case 'light intensity drizzle rain':
-      case 'drizzle rain':
-      case 'heavy intensity drizzle rain':
-      case 'shower rain and drizzle':
-      case 'heavy shower rain and drizzle':
-      case 'shower drizzle':
-      case 'shower rain':
-        imgCloud.src = './img/clouds/showerRain.png';
-        break;
-      case 'light rain':
-      case 'moderate rain':
-      case 'heavy intensity rain':
-      case 'very heavy rain':
-      case 'extreme rain':
-      case 'rain':
-        imgCloud.src = './img/clouds/rain.png';
-        break;
-      case 'thunderstorm with light rain':
-      case 'thunderstorm with rain':
-      case 'thunderstorm with heavy rain':
-      case 'light thunderstorm':
-      case 'thunderstorm':
-      case 'heavy thunderstorm':
-      case 'ragged thunderstorm':
-      case 'thunderstorm with light drizzle':
-      case 'thunderstorm with drizzle':
-      case 'thunderstorm with heavy drizzle':
-        imgCloud.src = './img/clouds/thunderstorm.png';
-        break;
-      case 'freezing rain':
-      case 'light snow':
-      case 'snow':
-      case 'heavy snow':
-      case 'sleet':
-      case 'light shower sleet':
-      case 'shower sleet':
-      case 'light rain and snow':
-      case 'rain and snow':
-      case 'light shower snow':
-      case 'shower snow':
-      case 'heavy shower snow':
-        imgCloud.src = './img/clouds/snow.png';
-        break;
-      case 'mist':
-      case 'smoke':
-      case 'haze':
-      case 'sand/dust whirls':
-      case 'fog':
-      case 'sand':
-      case 'dust':
-      case 'volcanic ash':
-      case 'squalls':
-      case 'tornado':
-        imgCloud.src = './img/clouds/mist.png';
-        break;
-    }
-}
-
-
-
 
 function now(event) {
   const url = `${serverUrl}?q=${cityName.value}&appid=${apiKey}`;
@@ -105,7 +22,7 @@ function now(event) {
       tempText.textContent = Math.round(temp.main.temp);
       cityNow.textContent = temp.name;
 
-      filter (temp.weather[0].description);
+      imgFilter (temp.weather[0].description, imgCloud);
       toStorageNow(cityNow);
       resolve()
     })
@@ -153,7 +70,7 @@ function getTemp(event) {
         tempText.textContent = Math.round(temp.main.temp);
         cityNow.textContent = temp.name;
 
-        filter (temp.weather[0].description);
+        imgFilter (temp.weather[0].description, imgCloud);
         toStorageNow(cityNow);
         resolve()
       })
@@ -181,7 +98,7 @@ function reloadPage() {
       tempText.textContent = Math.round(temp.main.temp);
       cityNow.textContent = temp.name;
 
-      filter (temp.weather[0].description);
+      imgFilter (temp.weather[0].description, imgCloud);
       toStorageNow(cityNow);
       resolve()
     })
