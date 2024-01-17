c679df12a3712c380f6a00b34635b6013dff96b4
Anton <you@example.com>
2024-01-17 16:59:07 +0300

    use asunc func in all code

--- Погода/main.js
+++ Погода/main.js
@@ -1,130 +1,135 @@
-import {toStorageFollow,toStorageNow, imgFilter} from "./module.js";
+import { toStorageFollow, toStorageNow, updateWeatherImg } from "./module.js";
 
-const serverUrl = 'http://api.openweathermap.org/data/2.5/weather';
-const cityName = document.querySelector('.search-input');
-const apiKey = 'f660a2fb1e4bad108d6160b7f58c555f&units=metric';
-const tempText = document.querySelector('.temp__text');
-const cityNow = document.querySelector('.city__now');
-const listCity = document.querySelector('.list__city');
-const followBtn = document.querySelector('.followBtn');
-const imgCloud = document.querySelector('.img__cloud');
+const serverUrl = "http://api.openweathermap.org/data/2.5/weather";
+const cityName = document.querySelector(".search-input");
+const apiKey = "f660a2fb1e4bad108d6160b7f58c555f&units=metric";
+const tempText = document.querySelector(".temp__text");
+const cityNow = document.querySelector(".city__now");
+const listCity = document.querySelector(".list__city");
+const followBtn = document.querySelector(".followBtn");
+const imgCloud = document.querySelector(".img__cloud");
+const listFollowCity = new Set();
 
-function now(event) {
+async function getCityTempFromInput() {
   const url = `${serverUrl}?q=${cityName.value}&appid=${apiKey}`;
-  const search = new Promise(async(resolve, reject) => {
-    let cityTemp = await fetch(url);
-    let temp = await cityTemp.json();
-    resolve(temp);
-  })
 
-  search.then(temp => {
-    return new Promise((resolve, reject) => {
-      tempText.textContent = Math.round(temp.main.temp);
-      cityNow.textContent = temp.name;
-
-      imgFilter (temp.weather[0].description, imgCloud);
-      toStorageNow(cityNow);
-      resolve()
-    })
-    
-  })
-  
-  .catch(error => {
-    alert('ошибка');
-    tempText.textContent = '-';
-    cityNow.textContent = 'Ошибка!';
-  });
-
-  event.preventDefault();
+  const responce = await fetch(url);
+  const temp = await responce.json();
+  return temp;
 }
 
-function followCity() {
-  if (listCity.childElementCount < 5){
-    listCity.insertAdjacentHTML('afterbegin',  `<div class="list__element">
+function updateTempVidget(tempData) {
+  const {weather,main,name} = tempData;
+  tempText.textContent = Math.round(main.temp);
+  cityNow.textContent = name;
+
+  updateWeatherImg(weather[0].description, imgCloud);
+}
+
+async function handleSubmitCity(event) {
+  event.preventDefault();
+
+  try {
+    const temp = await getCityTempFromInput();
+    updateTempVidget(temp);
+    toStorageNow(cityNow);
+  } catch (error) {
+    tempText.textContent = '-';
+    cityNow.textContent = 'Ошибка!';
+  }
+
+}
+
+function handleAddCityInFollowList() {
+  if (listCity.childElementCount < 5) {
+    listCity.insertAdjacentHTML(
+      "afterbegin",
+      `<div class="list__element">
     <p class="name__town">${cityNow.textContent}</p>
     <button>
     <img class="deleteBtn" src="img/delete.png" alt="0">
     </button>
-    </div>`);
+    </div>`
+    );
+    listFollowCity.add(cityNow.textContent);
   } else {
-    alert('Список полный!');
+    alert("Список полный!");
   }
-
-  toStorageFollow(listCity);
+  toStorageFollow(listFollowCity);
 }
 
-function getTemp(event) {
-  if (event.target.tagName != 'P') {
+async function getCityTempFromFollowList(event) {
+  const url = `${serverUrl}?q=${event.target.textContent}&appid=${apiKey}`;
+
+  const responce = await fetch(url);
+  const temp = await responce.json();
+  return temp;
+}
+
+async function handleGetDataFormFollowList(event) {
+  if (event.target.tagName != "P") {
     return;
-  } else{
-    const url = `${serverUrl}?q=${event.target.textContent}&appid=${apiKey}`;
-
-    const search = new Promise(async(resolve, reject) => {
-    let cityTemp = await fetch(url);
-    let temp = await cityTemp.json();
-    resolve(temp);
-    })
-
-    search.then(temp => {
-      return new Promise((resolve, reject) => {
-        tempText.textContent = Math.round(temp.main.temp);
-        cityNow.textContent = temp.name;
-
-        imgFilter (temp.weather[0].description, imgCloud);
-        toStorageNow(cityNow);
-        resolve()
-      })
-    
-    })
-  
-    .catch(error => {
-      alert('ошибка');
-      tempText.textContent = '-';
-      cityNow.textContent = 'Ошибка!';
-    });
+  } else {
+    try {
+      const temp = await getCityTempFromFollowList(event);
+      updateTempVidget(temp);
+      toStorageNow(cityNow);
+    } catch (error) {
+      tempText.textContent = "-";
+      cityNow.textContent = "Ошибка!";
+    }
   }
 }
 
-function reloadPage() {
-  const url = `${serverUrl}?q=${localStorage.getItem('city')}&appid=${apiKey}`;
-  const search = new Promise(async(resolve, reject) => {
-    let cityTemp = await fetch(url);
-    let temp = await cityTemp.json();
-    resolve(temp);
-  })
+async function getCityTempFromLocalStorage() {
+  const url = `${serverUrl}?q=${localStorage.getItem("city")}&appid=${apiKey}`;
 
-  search.then(temp => {
-    return new Promise((resolve, reject) => {
-      tempText.textContent = Math.round(temp.main.temp);
-      cityNow.textContent = temp.name;
+  const responce = await fetch(url);
+  const temp = await responce.json();
+  return temp;
+}
 
-      imgFilter (temp.weather[0].description, imgCloud);
-      toStorageNow(cityNow);
-      resolve()
-    })
+async function reloadPage() {
+  try {
+    const temp = await getCityTempFromLocalStorage();
+    updateTempVidget(temp);
+    toStorageNow(cityNow);
+
+    for (let cityName of JSON.parse(localStorage.getItem("follow"))) {
+      listFollowCity.add(cityName);
+      listCity.insertAdjacentHTML(
+        "afterbegin",
+        `<div class="list__element">
+    <p class="name__town">${cityName}</p>
+    <button>
+    <img class="deleteBtn" src="img/delete.png" alt="0">
+    </button>
+    </div>`
+      );
+    }
     
-  })
-  
-  .catch(error => {
-    alert('ошибка');
+  } catch (error) {
     tempText.textContent = '-';
     cityNow.textContent = 'Ошибка!';
-  });
+  }
 }
 
 function deleteCity(event) {
-  if (event.target.tagName != 'IMG') {
+  const deleteCity = event.target.parentNode.parentNode;
+  if (event.target.tagName != "IMG") {
     return;
   } else {
-    listCity.removeChild(event.target.parentNode.parentNode);
+    listFollowCity.delete( 
+      deleteCity.children[0].textContent
+    );
+    listCity.removeChild(deleteCity);
   }
-  toStorageFollow(listCity);
+  toStorageFollow(listFollowCity);
 }
 
-document.addEventListener('submit', now);
-followBtn.addEventListener('click', followCity);
-listCity.addEventListener('click', getTemp);
-listCity.addEventListener('click', deleteCity);
+document.addEventListener("submit", handleSubmitCity);
+followBtn.addEventListener("click", handleAddCityInFollowList);
+listCity.addEventListener("click", handleGetDataFormFollowList);
+listCity.addEventListener("click", deleteCity);
 
-listCity.innerHTML = localStorage.getItem('follow');
 reloadPage();
